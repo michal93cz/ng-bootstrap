7fbe65af669e1dddf9f7b3ca66df8dea634f91f5
Czupryniak, Michal <michal.czupryniak@pearson.com>
2017-01-08 15:06:47 +0100

    Multilevel nesting demo finish

--- demo/src/app/components/accordiontable/demos/_collapseicon/collapse-icon.component.ts
+++ demo/src/app/components/accordiontable/demos/_collapseicon/collapse-icon.component.ts
@@ -3,10 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'collapse-icon',
   template: `
-    <i (click)="isNotCollapsed=!isNotCollapsed"
-       [ngClass]="{'fa-chevron-up': isNotCollapsed, 'fa-chevron-down': !isNotCollapsed}"
-       class="fa fa-fw">
-    </i>
+    <div (click)="isNotCollapsed=!isNotCollapsed"
+         style="height: 25px; width: 35px;">
+      <i [ngClass]="{'fa-chevron-up': isNotCollapsed, 'fa-chevron-down': !isNotCollapsed}"
+         class="fa fa-fw">
+      </i>
+    </div>
   `
 })
+
 export class CollapseIconComponent {}
